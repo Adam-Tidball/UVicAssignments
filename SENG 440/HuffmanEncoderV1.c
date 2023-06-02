12d6ad5e6e3c0eb9666bbe90109ac6a307194eca
Adamt557 <abtidball@gmail.com>
2023-06-02 12:18:27 -0700

    trying to get the float prob to work

--- SENG 440/HuffmanEncoderV1.c
+++ SENG 440/HuffmanEncoderV1.c
@@ -2,28 +2,27 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-//define some constants used in the program
-#define MAX_LENGTH 250
+//define constant for maximum alphabet file size
+#define MAX_FILE_LENGTH 250
+#define MAX_SYMBOLS 63 //requires 6 bits of space for max symbol key 
 
 //data structer for the alphabet and probabilities
 struct Alpha_Cell{
-    char character;
-    float probability;
+    char character[MAX_SYMBOLS]; //this data struct only allows for char symbols
+    float probability[MAX_SYMBOLS];
 };
 
 
 int main(){
 
+    // Create variables and indicate the program is running
     printf("Huffman Decoding Starting Up!\n");
-
     struct Alpha_Cell alpha_cell;
+    char alphabet_input[MAX_FILE_LENGTH];
 
-    //open alphabet.txt file & end program if does not open
+    // Open alphabet.txt file & end the program if it does not open
     FILE *file;
-    char alphabet_input[MAX_LENGTH];
-
     file = fopen("alphabet.txt","r");
-
     if (file == NULL)
     {
         printf("Failed to open the file");
@@ -31,35 +30,57 @@ int main(){
     }
 
     // Read and print each line of the file
-    while (fgets(alphabet_input, MAX_LENGTH, file) != NULL) 
+    while (fgets(alphabet_input, MAX_FILE_LENGTH, file) != NULL) 
     {
         printf("Line being read: \"%s\"", alphabet_input);
 
-        // Each line gets parsed and put into a nice data structure
+        // Each line gets parsed and put into the data structure
         int i = 0;
+        int symbol_count = 0;
         while(alphabet_input[i] != '\0')
         {
+            
             if(isalpha(alphabet_input[i]))
             {
-                alpha_cell.character = alphabet_input[i];
+                alpha_cell.character[symbol_count] = alphabet_input[i];
+                i++;
             } 
             else if (isdigit(alphabet_input[i]))
             {
-                alpha_cell.probability = alphabet_input[i];
+                int x = 0;
+                char tmp_num[10];
+                while(isdigit(alphabet_input[i+x]) || ('.' == alphabet_input[i+x]) && x < 10)
+                {
+                    tmp_num[x] = alphabet_input[i+x];
+                    x++;
+                }
+                alpha_cell.probability[symbol_count] = atoi(tmp_num);
+                i = i + x;
+            } 
+            else 
+            {
+                i++;
             }
-            i++;
+            symbol_count++;
         }
-
+        
 
     }
     printf("All lines read\n");
 
-    //Test printing out of the data struct - FAILS HERE
-    printf("Index Number [%d] character: [%s] probability: [%f]\n", 1, alpha_cell.character, alpha_cell.probability);
-
-
     // Close the file
     fclose(file);
+
+
+    //print out the data from the data structure
+    printf("\nData from the data structure:\n");
+    int y = 0;
+    while(alpha_cell.character[y] != NULL)
+    {
+        printf("Character for index %d is: %c\n",y, alpha_cell.character[y]);
+        printf("Probability for index %d is: %f\n",y,alpha_cell.probability[y]);
+        y++;
+    }
 
     return 0;
 }
