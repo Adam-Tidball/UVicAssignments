57106846510be2e7d128e7ec769f8e8c15ce5404
Adamt557 <abtidball@gmail.com>
2023-06-08 12:31:53 -0700

    Salem created improvements to parsing section

--- SENG 440/HuffmanEncoderV1.c
+++ SENG 440/HuffmanEncoderV1.c
@@ -1,34 +1,70 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
 
-//define constant for maximum alphabet file size
+// Macros
 #define MAX_FILE_LENGTH 250
-#define MAX_SYMBOLS 63 //requires 6 bits of space for max symbol key 
+#define MAX_SYMBOLS 63 
 
-//data structer for the alphabet and probabilities
-struct Alpha_Cell{
-    char character[MAX_SYMBOLS]; //this data struct only allows for char symbols
-    float probability[MAX_SYMBOLS];
-};
+// Structs
+typedef struct {
+    char character;
+    float probability;
+} alphaCell;
+
+int parseAlphaProb(alphaCell* alphaList, FILE* fp){
+    // Acquire symbols with their respective probabilities from the input file
+    char alphabet_input[MAX_FILE_LENGTH];
+    fgets(alphabet_input, MAX_FILE_LENGTH, fp);
+
+    // Acquire first token from input; Splits on , and / as delimiters 
+    char *tokInput = strtok(alphabet_input, ",/");
+    alphaCell curCell;
+    int i = 0;
+
+    // Iterate through remaining tokens to populate list
+    while (tokInput){
+        if(isalpha(*tokInput)) {
+            curCell.character = *tokInput;
+            tokInput = strtok(NULL, ",/");
+            curCell.probability = atof(tokInput);
+
+            alphaList[i] = curCell;
+            i++;
+        }
+
+        tokInput = strtok(NULL, ",/");
+    }
+
+    return i;
+}
 
 
 int main(){
-
     // Create variables and indicate the program is running
     printf("Huffman Decoding Starting Up!\n");
-    struct Alpha_Cell alpha_cell;
-    char alphabet_input[MAX_FILE_LENGTH];
+    alphaCell alphaList[MAX_SYMBOLS];
 
     // Open alphabet.txt file & end the program if it does not open
-    FILE *file;
-    file = fopen("alphabet.txt","r");
-    if (file == NULL)
-    {
-        printf("Failed to open the file");
-        return 1;
+    FILE *fp;
+    fp = fopen("Alphabet.txt","r");
+    if (fp == NULL) {
+        printf("Failed to open the file!");
+        exit(-1);
     }
 
+    // Populates list with (symbol, probability) pairs
+    printf("Parsing file...\n");
+    int symbol_count = parseAlphaProb(alphaList, fp);
+    printf("Printing alphabet...\n");
+
+    for(int i = 0; i < symbol_count; i++){
+        printf("%c %.3f\n", alphaList[i].character, alphaList[i].probability);
+    }
+
+
+/*
     // Read and print each line of the file
     while (fgets(alphabet_input, MAX_FILE_LENGTH, file) != NULL) 
     {
@@ -36,32 +72,28 @@ int main(){
 
         // Each line gets parsed and put into the data structure
         int i = 0;
-        int symbol_count = 0;
         while(alphabet_input[i] != '\0')
         {
-            
             if(isalpha(alphabet_input[i]))
             {
                 alpha_cell.character[symbol_count] = alphabet_input[i];
-                i++;
+                symbol_count++;
             } 
             else if (isdigit(alphabet_input[i]))
             {
                 int x = 0;
-                char tmp_num[10];
-                while(isdigit(alphabet_input[i+x]) || ('.' == alphabet_input[i+x]) && x < 10)
+                char tmp_numS[10];
+                while(isdigit(alphabet_input[i+x]) || ('.' == alphabet_input[i+x]))
                 {
-                    tmp_num[x] = alphabet_input[i+x];
+                    tmp_numS[x] = alphabet_input[i+x];
                     x++;
                 }
-                alpha_cell.probability[symbol_count] = atoi(tmp_num);
+                alpha_cell.probability[prob_count] = strtof(tmp_numS, NULL);
+                printf("WHAT THE FYUCK IS RHIS %s\n",strtof(tmp_numS, NULL));
+                prob_count++;
                 i = i + x;
             } 
-            else 
-            {
-                i++;
-            }
-            symbol_count++;
+            i++;
         }
         
 
@@ -75,12 +107,12 @@ int main(){
     //print out the data from the data structure
     printf("\nData from the data structure:\n");
     int y = 0;
-    while(alpha_cell.character[y] != NULL)
+    while(6 > y)
     {
         printf("Character for index %d is: %c\n",y, alpha_cell.character[y]);
         printf("Probability for index %d is: %f\n",y,alpha_cell.probability[y]);
         y++;
     }
-
+*/
     return 0;
 }
